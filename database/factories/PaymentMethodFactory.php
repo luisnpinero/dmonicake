621f29f904450adcbbaf624b626c30ff4252fe45
Luis Enrique <luisnpinero@gmail.com>
2021-01-28 22:41:39 -0500

    migration y factory tablas -usuario

---
+++ database/factories/PaymentMethodFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PaymentMethod;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PaymentMethodFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PaymentMethod::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->randomElement(['Tarjeta de Débito', 'Tarjeta de Crédito','Transferencia Bancaria','Contra entrega','Paypal']),
+            'status'  => $this->faker->randomElement(['Active','Inactive']),
+            'modified_by' => $this->faker->randomDigitNotNull,
+        ];
+    }
+}
